1e4458987a7590e146fd184b8c1b06673ff82b22
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2025-01-07 20:50:27 +0700

    getsession

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <script lang="ts" setup>
-const { activeError } = storeToRefs(useErrorStore())
+import { supabase } from './lib/supabaseClient'
+
+const errorStore = useErrorStore()
+const authStore = useAuthStore()
+
+onErrorCaptured((error) => {
+  errorStore.setError({ error })
+})
+
+onMounted(async () => {
+  const { data } = await supabase.auth.getSession()
+  if (data.session?.user) {
+    await authStore.setAuth(data.session)
+  }
+})
 </script>
 
 <template>
   <AuthLayout>
-    <AppErrorPage v-if="activeError" />
+    <AppErrorPage v-if="errorStore.activeError" />
+
     <RouterView v-slot="{ Component, route }">
       <Suspense v-if="Component" :timeout="0">
         <Component :is="Component" :key="route.name" />
