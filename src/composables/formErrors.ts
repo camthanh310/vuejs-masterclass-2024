af7f943084167b6bf21a13f018b046ad751df0d2
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2025-01-10 20:42:43 +0700

    realtime errors

--- src/composables/formErrors.ts
+++ src/composables/formErrors.ts
@@ -1,15 +1,38 @@
+import type { LoginForm } from '@/types/AuthForm'
 import type { AuthError } from '@supabase/supabase-js'
 
 export function useFormErrors() {
   const serverError = ref('')
+  const realtimeErrors = ref()
 
   function handleServerError(error: AuthError) {
     serverError.value =
       error.message === 'Invalid login credentials' ? 'Incorrect email or password' : error.message
   }
 
+  async function handleLoginForm(formData: LoginForm) {
+    realtimeErrors.value = {
+      email: [],
+      password: [],
+    }
+
+    const { validateEmail, validatePassword } = await import('@/utils/formValidations')
+
+    const emailErrors = validateEmail(formData.email)
+    if (emailErrors.length) {
+      realtimeErrors.value.email = emailErrors
+    }
+
+    const passwordErrors = validatePassword(formData.password)
+    if (passwordErrors.length) {
+      realtimeErrors.value.password = passwordErrors
+    }
+  }
+
   return {
     serverError,
     handleServerError,
+    realtimeErrors,
+    handleLoginForm,
   }
 }
