f9e6acb78d8d2f5c7595c799c90797af213fd269
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2025-01-10 20:46:38 +0700

    watch debounce

--- src/utils/formValidations.ts
+++ src/utils/formValidations.ts
@@ -20,5 +20,7 @@ export function validatePassword(password: string) {
 
   if (password.length <= 6) errors.push('Password must be more than 6 characters')
 
+  if (!password.includes('@')) errors.push('Password must include special characters')
+
   return errors
 }
