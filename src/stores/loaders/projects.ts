76176c7743c65493f21cadd7cd4c682e94114e58
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2025-01-13 20:48:23 +0700

    update stale with fresh

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -2,7 +2,7 @@ import { projectsQuery, type Projects } from '@/utils/supaQueries'
 import { useMemoize } from '@vueuse/core'
 
 export const useProjectsStore = defineStore('projects-store', () => {
-  const projects = ref<Projects | null>(null)
+  const projects = ref<Projects>([])
   const loadProjects = useMemoize(async (key: string) => await projectsQuery)
 
   async function getProjects() {
@@ -12,19 +12,23 @@ export const useProjectsStore = defineStore('projects-store', () => {
       useErrorStore().setError({ error, customCode: status })
     }
 
-    projects.value = data
+    if (data) {
+      projects.value = data
+    }
 
     validateCache()
   }
 
   function validateCache() {
     if (projects.value?.length) {
-      projectsQuery.then(({ data }) => {
+      projectsQuery.then(({ data, error }) => {
         if (JSON.stringify(projects.value) === JSON.stringify(data)) {
-          console.log('Cached and fresh data matched!')
+          //
         } else {
-          console.log('Something has changed!')
           loadProjects.delete('projects')
+          if (!error && data) {
+            projects.value = data
+          }
         }
       })
     }
