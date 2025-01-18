cf2ee2d4114700660e50b4bdde16ab10857cdf30
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2025-01-18 20:37:02 +0700

    guest layout

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,16 @@ onErrorCaptured((error) => {
 onMounted(() => {
   useAuthStore().trackAuthChanges()
 })
+
+const { user } = storeToRefs(useAuthStore())
+
+const AuthLayout = defineAsyncComponent(() => import('@/components/Layout/main/AuthLayout.vue'))
+
+const GuestLayout = defineAsyncComponent(() => import('@/components/Layout/main/GuestLayout.vue'))
 </script>
 
 <template>
-  <AuthLayout>
+  <Component :is="user ? AuthLayout : GuestLayout">
     <AppErrorPage v-if="errorStore.activeError" />
 
     <RouterView v-slot="{ Component, route }">
@@ -23,5 +29,5 @@ onMounted(() => {
         </template>
       </Suspense>
     </RouterView>
-  </AuthLayout>
+  </Component>
 </template>
